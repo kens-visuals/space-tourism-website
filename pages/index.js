62356f07368d86c3928c80c4228761f624db47b5
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-04-08 17:42:04 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -12,6 +12,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -65,5 +67,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
